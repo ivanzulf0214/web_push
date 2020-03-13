c3a4ceb907c12012a6681b7843982277f1edca98
Muhamad Ivanda ZUlfikar <ivanda.zulfikar@fdvs.io>
2020-03-13 11:04:07 +0700

    fix

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:ivanda.zulfikar@fdvs.io', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+	"endpoint":"https://fcm.googleapis.com/fcm/send/ds8bIUMpaaE:APA91bE25aHDGmyQDY_UwG0suF_MsdYCKo_PTr7M5G1CDEft8Ilvd24ZCsTeKLBC5oW_DQP065WD1OrR2auICsvP5xQzMRVNs4KQvJdqZ9G8n9JtZIBVFAliJXjnZpi9our635ouE7tJ",
+	"expirationTime":null,
+	"keys":{
+		"p256dh":"BLfJNKgA_v1cM1t3wdtQW2PD7uPEbdpGzXbkTuqEuEleY0cRh9Kfzz8hi_pIDkw_rUPoYoRlAbJ0JctBy3UIenc",
+		"auth":"DxMVaFeM8nozxtwE4xlsFw"
+	}
+};
 
 push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('push', () => {
-	self.registration.sendNotification('test message', {})
+self.addEventListener('push', function(e) {
+	var options = {
+		body: 'Here is a notification body!',
+		icon: 'images/example.png',
+		vibrate: [100, 50, 100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: 1
+		},
+		actions: [
+			{
+				action: 'explore',
+				title: 'Explore this new world',
+				icon: 'images/checkmark.png'
+			},
+			{
+				action: 'close',
+				title: 'CLose notification',
+				icon: 'images/xmark.png'
+			}
+		]
+	};
+
+	e.waitUntil(self.registration.showNotification('Hello World!', options));
 });
